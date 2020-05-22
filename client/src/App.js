35def01736b5151a5c4466d7e2de044f1ce64839
Riley-Robinson <rrtoinfinity@gmail.com>
2020-05-22 12:28:57 -0400

    App.js done woot we have bubbles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./Utils/PrivateRoute";
 import BubblePage from "./components/BubblePage";
 
@@ -10,11 +10,18 @@ function App() {
 	return (
 		<Router>
 			<div className="App">
-				<Route exact path="/" component={Login} />
-				{/* 
+				<Switch>
+					<Route exact path="/" component={Login} />
+					{/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+					<PrivateRoute
+						exact
+						path="/bubblepage"
+						component={BubblePage}
+					/>
+				</Switch>
 			</div>
 		</Router>
 	);
